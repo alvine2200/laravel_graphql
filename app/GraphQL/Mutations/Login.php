20c3293dba07841a2512ab015381c831f1999a36
alvine2200 <alvinellavu@gmail.com>
2023-10-25 13:29:05 +0300

    user login token returned

---
+++ app/GraphQL/Mutations/Login.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+final readonly class Login
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        $user = User::where('email', $args["email"])->first();
+
+        if (!$user || !Hash::check($args["password"], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken("Web123@-yq54%")->plainTextToken; 
+    }
+}
